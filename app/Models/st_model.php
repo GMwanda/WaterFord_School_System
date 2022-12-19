5105b3ff029e80538d5eafb91acf877a5d4d1f74
ashir245 <117296966+ashir245@users.noreply.github.com>
2022-12-19 06:37:49 -0800

    Add files via upload

---
+++ app/Models/st_model.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class st_model extends Model
+{
+    protected $table='students_profile';
+    use HasFactory;
+}
