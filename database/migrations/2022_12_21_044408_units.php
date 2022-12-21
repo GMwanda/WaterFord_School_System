ac0cdd1ca7db0abef8c1b9ce1e2add04c9dfee74
ashir245 <117296966+ashir245@users.noreply.github.com>
2022-12-21 10:11:42 -0800

    Add files via upload

---
+++ database/migrations/2022_12_21_044408_units.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->id();
+            $table->string('Name', 100);
+            $table->string('Class', 50);
+            $table->string('units', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
