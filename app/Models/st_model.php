d66ae438f31dc6c66304d0254b636a9ac5600263
ashir245 <117296966+ashir245@users.noreply.github.com>
2022-12-25 04:41:00 -0800

    Update st_model.php

--- app/Models/st_model.php
+++ app/Models/st_model.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class st_model extends Model
-{
+{   use HasFactory;
     protected $table='students_profile';
     public  $timestamps=false;
 }
