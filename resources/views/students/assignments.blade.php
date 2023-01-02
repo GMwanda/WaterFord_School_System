22016bec052d140c8e55f73a181e90799d93a6b2
Dave <70270638+davidMorekwa@users.noreply.github.com>
2023-01-02 15:02:52 +0300

    Changes

--- resources/views/students/assignments.blade.php
+++ resources/views/students/assignments.blade.php
@@ -1,1 +1,50 @@
-<link rel="stylesheet" type="text"  href="{{asset('storage') }}">
+{{-- <link rel="stylesheet" type="text"  href="{{asset('storage') }}"> --}}
+@extends('students.studentlayout')
+@section('std_content')
+<head>
+
+
+<link rel="stylesheet" href="/ProgramStyling/app.css">
+</head>
+
+  
+   
+    <h3 class="text text-primary text-center mt-5">assignments</h3>
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                          <th>title</th>
+                          <th>Description</th>
+                          <th>Due_date</th>
+                          <th>file</th>
+                         
+
+                    </tr>
+                </thead>
+                <tbody>
+                     @foreach ($as as $s)
+                         
+                    
+                    <tr>
+                        
+                        <td>{{$s->Title }}</td>
+                        <td>{{$s->Description}} </td>
+                        <td>{{$s->Due_Date}} </td>
+                        {{-- <td>{{asset('Assignments/'.{{$s->AssignmentFile}})}}</td> --}}
+                        <td>@php
+                            $f = $s->AssignmentFile;
+                            $v = Storage::url($f);
+                        @endphp
+                        <a href="{{$v}}">{{$v}}</a>
+                        </td>
+                          
+               
+                    </tr>
+                    @endforeach  
+                </tbody>
+            </table>
+        </div>
+    </div>
+    @endsection
